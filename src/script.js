9c0747812eb68ee8369f553dbb305a9ff8e36919
Harsh <unagctystudent2@gmail.com>
2020-04-13 19:52:58 -0400

    mobile dropdown equivalent

--- src/script.js
+++ src/script.js
@@ -1,12 +1,23 @@
 function changeNav() {
   if (getComputedStyle(document.getElementsByTagName("nav")[1]).width == "0px") {
-    document.getElementsByTagName("nav")[1].style.width = "250px";
-    document.getElementsByTagName("nav")[1].style.paddingLeft = "56px";
+    document.getElementsByTagName("nav")[1].style.width = "150px";
+    document.getElementsByTagName("nav")[1].style.paddingLeft = "50px";
     document.getElementById("content").style.pointerEvents = "none";
   } else {
     document.getElementsByTagName("nav")[1].style.width = "0px";
     document.getElementsByTagName("nav")[1].style.paddingLeft = "0px";
+    document.getElementById("slide").style.left = "0px";
+    document.getElementById("slide").style.width = "0px";
+    document.getElementById("slide").innerHTML = "";
     document.getElementById("content").style.pointerEvents = "auto";
+  }
+}
+
+function orderNav() {
+  if (getComputedStyle(document.getElementsByClassName("right")[0]).float == "left") {
+    document.getElementsByTagName("nav")[0].innerHTML = "<a href =\"vehicles/\" class=\"left dropdowner\" onmouseover=\"dropdown(0)\">Vehicles</a> <a href =\"charging/\" class=\"left dropdowner\" onmouseover=\"dropdown(1)\">Charging</a> <a href=\"mission/\" class=\"left\">Mission</a> <a href=\"news/\" class=\"left\">News</a> <a href=\"shop/\" class=\"right\">Shop</a> <a href=\"account/login.html\" class=\"right\">Log In</a> <a href=\"account/register.html\" class=\"right\">Register</a> <div class=\"dropdown forzero\"> <a href=\"vehicles/alpine.html\">Alpine</a> <a href=\"vehicles/boulder.html\">Boulder</a> <a href=\"vehicles/dune.html\">Dune</a> <a href=\"vehicles/glacier.html\">Glacier</a> <a href=\"vehicles/icicle.html\">Icicle</a> <a href=\"vehicles/neptune.html\">Neptune</a> <a href=\"vehicles/taiga.html\">Taiga</a> </div> <div class=\"dropdown forone\"> <a href=\"charging/chargers.html\">Chargers</a> <a href=\"charging/maps.html\">Map</a> </div>";
+  } else {
+    document.getElementsByTagName("nav")[0].innerHTML = "<a href =\"vehicles/\" class=\"left dropdowner\" onmouseover=\"dropdown(0)\">Vehicles</a> <a href =\"charging/\" class=\"left dropdowner\" onmouseover=\"dropdown(1)\">Charging</a> <a href=\"mission/\" class=\"left\">Mission</a> <a href=\"news/\" class=\"left\">News</a> <a href=\"account/register.html\" class=\"right\">Register</a> <a href=\"account/login.html\" class=\"right\">Log In</a> <a href=\"shop/\" class=\"right\">Shop</a> <div class=\"dropdown forzero\"> <a href=\"vehicles/alpine.html\">Alpine</a> <a href=\"vehicles/boulder.html\">Boulder</a> <a href=\"vehicles/dune.html\">Dune</a> <a href=\"vehicles/glacier.html\">Glacier</a> <a href=\"vehicles/icicle.html\">Icicle</a> <a href=\"vehicles/neptune.html\">Neptune</a> <a href=\"vehicles/taiga.html\">Taiga</a> </div> <div class=\"dropdown forone\"> <a href=\"charging/chargers.html\">Chargers</a> <a href=\"charging/maps.html\">Map</a> </div>";
   }
 }
 
@@ -53,4 +64,24 @@ function mousedOuttaZero(event) {
 
 function mousedOuttaOne(event) {
   mousedOutta(event, 1);
+}
+
+function slideNav(index) {
+  var images = document.getElementsByTagName("nav")[1].getElementsByTagName("img");
+  if ((getComputedStyle(document.getElementById("slide")).width == "0px") || (document.getElementsByTagName("nav")[1].getElementsByTagName("img")[index].src.endsWith("img/right.png"))) {
+    for (var i = 0; i < images.length; i++) {
+      images[i].src = "img/right.png";
+    }
+    document.getElementsByTagName("nav")[1].getElementsByTagName("img")[index].src = "img/left.png";
+    document.getElementById("slide").style.left = "200px";
+    document.getElementById("slide").style.width = "100px";
+    document.getElementById("slide").innerHTML = document.getElementsByTagName("nav")[0].getElementsByClassName("dropdown")[index].innerHTML;
+  } else {
+    for (var i = 0; i < images.length; i++) {
+      images[i].src = "img/right.png";
+    }
+    document.getElementById("slide").style.width = "0px";
+    document.getElementById("slide").style.left = "0px";
+    document.getElementById("slide").innerHTML = "";
+  }
 }
